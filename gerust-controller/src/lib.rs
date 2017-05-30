593b50ce19cc42d6523260188fb8b1c525663a47
Florian Gilcher <florian.gilcher@asquera.de>
2017-05-30 16:03:53 +0200

    Testing the Controller protocoll

--- gerust-controller/src/lib.rs
+++ gerust-controller/src/lib.rs
@@ -1,28 +1,48 @@
 extern crate gerust_context;
 
 use gerust_context::Context;
+use std::marker::PhantomData;
 
-pub trait ControllerFactory<C: Context> {
-    type Controller: Controller<C>;
+pub fn construct<Ctx: Context + 'static>() -> FactoryFactory<Ctx> {
+    FactoryFactory::new()
+}
 
-    fn produce(&self, c: &C) -> Box<Self::Controller> {
-        Box::new(Controller::new(c))
+pub struct FactoryFactory<Cont: Context + 'static> {
+    p: PhantomData<&'static Cont>
+}
+
+impl<Cont: Context + 'static> FactoryFactory<Cont> {
+    fn new() -> Self { Self { p: PhantomData::default() }}
+
+    pub fn for_controller<Control: Controller<'static, Cont>>(&self) -> Factory<'static, Cont, Control>  {
+        Factory::new()
     }
 }
 
-pub trait Controller<C: Context> {
+pub struct Factory<'a, Cont: Context + 'a, Control: Controller<'a, Cont> + 'a> {
+    c: PhantomData<&'a Cont>,
+    p: PhantomData<&'a Control>
+}
+
+impl<'a, Cont: Context + 'a, Control: Controller<'a, Cont> + 'a> Factory<'a, Cont, Control> {
+    fn new() -> Self { Self { c: PhantomData::default(), p: PhantomData::default() }}
+}
+
+impl<'a, 'b, Ctx: Context + 'b, Cont: Controller<'b, Ctx> + 'b> ControllerFactory<'a, 'b, Ctx, Cont> for Factory<'b, Ctx, Box<Cont>> {
+
+}
+
+pub trait ControllerFactory<'a, 'b, Ctx: Context + 'b, Cont: Controller<'b, Ctx> + 'b > {
+    fn produce(&'a self, c: &'b Ctx) -> Box<Controller<'b, Ctx>> {
+        Box::new(Cont::new(c))
+    }
+}
+
+pub trait Controller<'a, C: Context + 'a> {
     type Params;
     type Result;
 
-    fn new(c: &C) -> Self where Self: Sized;
-}
+    fn new(c: &'a C) -> Self where Self: Sized;
 
-#[allow(dead_code)]
-struct __assert_object_safety<T: ?Sized> {
-    inner: Box<T>
+    fn execute(&self, input: Self::Params) -> Self::Result;
 }
-
-#[allow(dead_code)]
-type checker<C, P, R> = __assert_object_safety<Controller<C, Params=P, Result=R>>;
-#[allow(dead_code)]
-type checker_s<C, P, R> = __assert_object_safety<ControllerFactory<C, Controller=Controller<C, Params=P, Result=R>>>;
