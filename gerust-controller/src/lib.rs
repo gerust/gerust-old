101280d50f3708a974f566346cdf1d5bfcd56dd1
Florian Gilcher <florian.gilcher@asquera.de>
2017-06-03 17:18:59 +0300

    Well, this works, too

--- gerust-controller/src/lib.rs
+++ gerust-controller/src/lib.rs
@@ -3,41 +3,6 @@ extern crate gerust_context;
 use gerust_context::Context;
 use std::marker::PhantomData;
 
-pub fn construct<Ctx: Context + 'static>() -> FactoryFactory<Ctx> {
-    FactoryFactory::new()
-}
-
-pub struct FactoryFactory<Cont: Context + 'static> {
-    p: PhantomData<&'static Cont>
-}
-
-impl<Cont: Context + 'static> FactoryFactory<Cont> {
-    fn new() -> Self { Self { p: PhantomData::default() }}
-
-    pub fn for_controller<Control: Controller<'static, Cont>>(&self) -> Factory<'static, Cont, Control>  {
-        Factory::new()
-    }
-}
-
-pub struct Factory<'a, Cont: Context + 'a, Control: Controller<'a, Cont> + 'a> {
-    c: PhantomData<&'a Cont>,
-    p: PhantomData<&'a Control>
-}
-
-impl<'a, Cont: Context + 'a, Control: Controller<'a, Cont> + 'a> Factory<'a, Cont, Control> {
-    fn new() -> Self { Self { c: PhantomData::default(), p: PhantomData::default() }}
-}
-
-impl<'a, 'b, Ctx: Context + 'b, Cont: Controller<'b, Ctx> + 'b> ControllerFactory<'a, 'b, Ctx, Cont> for Factory<'b, Ctx, Box<Cont>> {
-
-}
-
-pub trait ControllerFactory<'a, 'b, Ctx: Context + 'b, Cont: Controller<'b, Ctx> + 'b > {
-    fn produce(&'a self, c: &'b Ctx) -> Box<Controller<'b, Ctx>> {
-        Box::new(Cont::new(c))
-    }
-}
-
 pub trait Controller<'a, C: Context + 'a> {
     type Params;
     type Result;
@@ -45,4 +10,22 @@ pub trait Controller<'a, C: Context + 'a> {
     fn new(c: &'a C) -> Self where Self: Sized;
 
     fn execute(&self, input: Self::Params) -> Self::Result;
+}
+
+pub struct Factory<'a, 'b, C: Context + 'b, Params, Result, T: Controller<'b, C, Params=Params, Result=Result> + 'static> {
+    inner: PhantomData<(&'a (), &'b (), C, Params, Result, T)>
+}
+
+pub fn factory<'a, 'b, C: Context + 'b, Params, Result, T: Controller<'b, C, Params=Params, Result=Result> + 'static>() -> Factory<'a, 'b, C, Params, Result, T> {
+    Factory { inner: PhantomData::default() }
+}
+
+impl<'a, 'b, C: Context + 'b, Params, Result, T: Controller<'b, C, Params=Params, Result=Result> + 'static> ControllerFactory<'a, 'b, C, Params, Result> for Factory<'a, 'b, C, Params, Result, T> {
+    fn produce(&'a self, c: &'b C) -> Box<Controller<'b, C, Params=Params, Result=Result>> {
+        Box::new(T::new(c))
+    }
+}
+
+pub trait ControllerFactory<'a, 'b, C: Context + 'b, Params, Result> {
+    fn produce(&'a self, c: &'b C) -> Box<Controller<'b, C, Params=Params, Result=Result>>;
 }
