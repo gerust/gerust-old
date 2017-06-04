a62430d4d2f8e07260c0b9a7ec2d6183b7182a36
Florian Gilcher <florian.gilcher@asquera.de>
2017-06-04 17:33:24 +0300

    Introduce ControllerSet

--- gerust-controller/src/lib.rs
+++ gerust-controller/src/lib.rs
@@ -12,15 +12,16 @@ pub trait Controller<'a, C: Context + 'a> {
     fn execute(&self, input: Self::Params) -> Self::Result;
 }
 
-pub struct Factory<'a, 'b, C: Context + 'b, Params, Result, T: Controller<'b, C, Params=Params, Result=Result> + 'static> {
+pub struct Factory<'a, 'b, C: Context + 'b, Params, Result, T: Controller<'b, C, Params=Params, Result=Result>> {
     inner: PhantomData<(&'a (), &'b (), C, Params, Result, T)>
 }
 
-pub fn factory<'a, 'b, C: Context + 'b, Params, Result, T: Controller<'b, C, Params=Params, Result=Result> + 'static>() -> Factory<'a, 'b, C, Params, Result, T> {
+pub fn factory<'a, 'b, C: Context + 'b, T: Controller<'b, C> + 'static>() -> Factory<'a, 'b, C, T::Params, T::Result, T> {
     Factory { inner: PhantomData::default() }
 }
 
 impl<'a, 'b, C: Context + 'b, Params, Result, T: Controller<'b, C, Params=Params, Result=Result> + 'static> ControllerFactory<'a, 'b, C, Params, Result> for Factory<'a, 'b, C, Params, Result, T> {
+
     fn produce(&'a self, c: &'b C) -> Box<Controller<'b, C, Params=Params, Result=Result>> {
         Box::new(T::new(c))
     }
@@ -29,3 +30,22 @@ impl<'a, 'b, C: Context + 'b, Params, Result, T: Controller<'b, C, Params=Params
 pub trait ControllerFactory<'a, 'b, C: Context + 'b, Params, Result> {
     fn produce(&'a self, c: &'b C) -> Box<Controller<'b, C, Params=Params, Result=Result>>;
 }
+
+pub struct ControllerSet<'factories, 'controllers, C: Context, Result, Params> {
+    controllers: Vec<Box<ControllerFactory<'factories, 'controllers, C, Params, Result>> >,
+}
+
+impl<'factories, 'controllers, C: Context + 'controllers, Params, Result> ControllerSet<'factories, 'controllers, C, Result, Params> {
+    pub fn new() -> Self {
+        ControllerSet { controllers: Vec::new() }
+    }
+
+    pub fn register_controller(&mut self, factory: Box<ControllerFactory<'factories, 'controllers, C, Params, Result>>) {
+        self.controllers.push(factory)
+    }
+
+    pub fn controllers(&self) -> &[Box<ControllerFactory<'factories, 'controllers, C, Params, Result> + 'factories>] {
+        self.controllers.as_ref()
+    }
+}
+
