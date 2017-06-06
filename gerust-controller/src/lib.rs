b086d48cbe84aa31ca96ff363a62da9bac6d17f8
Florian Gilcher <florian.gilcher@asquera.de>
2017-06-06 09:33:37 +0200

    Make Context 'static and simplify a lot of things

--- gerust-controller/src/lib.rs
+++ gerust-controller/src/lib.rs
@@ -3,48 +3,48 @@ extern crate gerust_context;
 use gerust_context::Context;
 use std::marker::PhantomData;
 
-pub trait Controller<'a, C: Context + 'a> {
+pub trait Controller<C: Context> {
     type Params;
     type Result;
 
-    fn new(c: &'a C) -> Self where Self: Sized;
+    fn new(c: &'static C) -> Self where Self: Sized;
 
     fn execute(&self, input: Self::Params) -> Self::Result;
 }
 
-pub struct Factory<'a, 'b, C: Context + 'b, Params, Result, T: Controller<'b, C, Params=Params, Result=Result>> {
-    inner: PhantomData<(&'a (), &'b (), C, Params, Result, T)>
+pub struct Factory<C: Context + 'static, Params, Result, T: Controller<C, Params=Params, Result=Result>> {
+    inner: PhantomData<(C, Params, Result, T)>
 }
 
-pub fn factory<'a, 'b, C: Context + 'b, T: Controller<'b, C> + 'static>() -> Factory<'a, 'b, C, T::Params, T::Result, T> {
+pub fn factory<C: Context, T: Controller<C> + 'static>() -> Factory<C, T::Params, T::Result, T> {
     Factory { inner: PhantomData::default() }
 }
 
-impl<'a, 'b, C: Context + 'b, Params, Result, T: Controller<'b, C, Params=Params, Result=Result> + 'static> ControllerFactory<'a, 'b, C, Params, Result> for Factory<'a, 'b, C, Params, Result, T> {
+impl< C: Context, Params, Result, T: Controller<C, Params=Params, Result=Result> + 'static> ControllerFactory<C, Params, Result> for Factory<C, Params, Result, T> {
 
-    fn produce(&'a self, c: &'b C) -> Box<Controller<'b, C, Params=Params, Result=Result>> {
+    fn produce(&self, c: &'static C) -> Box<Controller<C, Params=Params, Result=Result>> {
         Box::new(T::new(c))
     }
 }
 
-pub trait ControllerFactory<'a, 'b, C: Context + 'b, Params, Result> {
-    fn produce(&'a self, c: &'b C) -> Box<Controller<'b, C, Params=Params, Result=Result>>;
+pub trait ControllerFactory<C: Context, Params, Result> {
+    fn produce(&self, c: &'static C) -> Box<Controller<C, Params=Params, Result=Result>>;
 }
 
-pub struct ControllerSet<'factories, 'controllers, C: Context, Result, Params> {
-    controllers: Vec<Box<ControllerFactory<'factories, 'controllers, C, Params, Result>> >,
+pub struct ControllerSet<C: Context, Result, Params> {
+    controllers: Vec<Box<ControllerFactory<C, Params, Result>> >,
 }
 
-impl<'factories, 'controllers, C: Context + 'controllers, Params, Result> ControllerSet<'factories, 'controllers, C, Result, Params> {
+impl<C: Context, Params: 'static, Result: 'static> ControllerSet<C, Result, Params> {
     pub fn new() -> Self {
         ControllerSet { controllers: Vec::new() }
     }
 
-    pub fn register_controller(&mut self, factory: Box<ControllerFactory<'factories, 'controllers, C, Params, Result>>) {
-        self.controllers.push(factory)
+    pub fn register_controller<T: Controller<C, Params=Params, Result=Result> + 'static>(&mut self) {
+        self.controllers.push(Box::new(factory::<C, T>()))
     }
 
-    pub fn controllers(&self) -> &[Box<ControllerFactory<'factories, 'controllers, C, Params, Result> + 'factories>] {
+    pub fn controllers(&self) -> &[Box<ControllerFactory<C, Params, Result>>] {
         self.controllers.as_ref()
     }
 }
